9ca5292a5eb1aafcc482a3a9bacedebff9dc712b
troglodisme <giulio.ammendola@network.rca.ac.uk>
2022-12-12 11:45:46 +0100

    Initial Commit

---
+++ ARescue/ARescueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARescueApp.swift
+//  ARescue
+//
+//  Created by Giulio on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ARescueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
